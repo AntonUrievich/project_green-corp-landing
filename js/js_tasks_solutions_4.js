22e0259bd3c27fecffbebec8ed901dd1a255fa59
AntonUrievich <antonurievi4@gmail.com>
2023-01-27 13:47:56 +0400

    start new tasks

---
+++ js/js_tasks_solutions_4.js
@@ -0,0 +1,7 @@
+/* 1.1 Напишите скрипт, который определяет тип тега с классом secret и выводит его в консоль.
+Пример <div class="secret">...</div>
+Результат div */
+
+console.log(document.querySelector(".secret").tagName.toLowerCase())
+
+/* 1.2  */
